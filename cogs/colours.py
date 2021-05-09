620f507c1cb4cfb3c62ca8cb0ece0ccfd016d746
Rocked03 <rockedohthree@gmail.com>
2021-05-09 23:20:23 +1200

    SSL handshake attempted fix

--- cogs/colours.py
+++ cogs/colours.py
@@ -18,7 +18,7 @@ class ColoursCog(commands.Cog, name="Colours"):
     def __init__(self, bot):
         self.bot = bot
 
-        a = pymongo.MongoClient("mongodb+srv://Rocked03:eem8yFOpEnm5dW1Y@blurple-canvas.lj40x.mongodb.net/test?retryWrites=true&w=majority")
+        a = pymongo.MongoClient("mongodb+srv://Rocked03:eem8yFOpEnm5dW1Y@blurple-canvas.lj40x.mongodb.net/test?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
         self.colourscoll = a.colours
 
         self.bot.colours, self.bot.coloursrgb, self.bot.coloursdict = self.defaultcolours()
