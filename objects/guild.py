018b81991e3cbb4725b8142f08a8027a83a56b93
Rocked03 <rockedohthree@gmail.com>
2024-05-07 09:18:02 +1200

    broken invites

--- objects/guild.py
+++ objects/guild.py
@@ -18,7 +18,7 @@ class Guild(DiscordObject):
         super().__init__(**kwargs)
         self.id = _id
         self.manager_role_id = manager_role
-        self.invite = invite.lstrip("https://").rstrip("/") if invite else invite
+        self.invite = invite.replace("https://", "").rstrip("/") if invite else invite
 
         self.guild: Optional[GuildDiscord] = None
         self.manager_role: Optional[Role] = None
