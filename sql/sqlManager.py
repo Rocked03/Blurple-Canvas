467f6e36f88dde9ddcf3865c2abcc7d862f6975f
Rocked03 <rockedohthree@gmail.com>
2024-05-08 04:33:05 +1200

    hotfix 2

--- sql/sqlManager.py
+++ sql/sqlManager.py
@@ -436,7 +436,7 @@ class SQLManager:
         user_id = user.id if user_id is None else user_id
         row = await self.conn.fetchrow(
             "SELECT u.*, b.date_added, "
-            "c.name, c.locked, c.event_id, c.width, c.height, c.cooldown_length,"
+            "c.name, c.locked, c.event_id, c.width, c.height, c.cooldown_length "  # , "
             # "d.username, d.profile_picture_url, d.user_id as discord_user_id "
             "FROM (SELECT * FROM public.user WHERE public.user.id = $1) u "
             "LEFT JOIN blacklist b ON u.id = b.user_id "
