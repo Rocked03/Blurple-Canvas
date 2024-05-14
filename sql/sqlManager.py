27e52f8e151ad4a2f2b2f7399c977698558d207f
Rocked03 <rockedohthree@gmail.com>
2024-05-15 01:59:51 +1200

    no new accounts at all

--- sql/sqlManager.py
+++ sql/sqlManager.py
@@ -527,7 +527,7 @@ class SQLManager:
 
         new_users_allowed = False
         if not new_users_allowed:
-            if user.id > 1233780098536509452:  # April 27, 2024
+            if user.id > 1233780098536509452 or True:  # April 27, 2024
                 await self.conn.execute(
                     "INSERT INTO blacklist (user_id) VALUES ($1)", user.id
                 )
